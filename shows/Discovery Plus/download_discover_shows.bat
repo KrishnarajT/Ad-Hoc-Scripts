7e0ea444b1bdb6828784ae7dffda8cfbec050cb0
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2025-03-26 00:38:17 +0530

    added mahabharat files

---
+++ shows/Discovery Plus/download_discover_shows.bat
@@ -0,0 +1,27 @@
+@echo off
+echo Running commands...
+
+:: Run each command
+.\yt-dlp "https://www.discoveryplus.in/show/seven-worlds-one-planet-in" --cookies .\dpcokie.txt -P "S:\Documentaries\seven-worlds-one-planet-in" -f 'bv+hls-stereo_128000-English'
+.\yt-dlp "https://www.discoveryplus.in/show/natures-great-events-in" --cookies .\dpcokie.txt -P "S:\Documentaries\natures-great-events-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/the-green-planet-in" --cookies .\dpcokie.txt -P "S:\Documentaries\the-green-planet-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/earths-tropical-islands-in" --cookies .\dpcokie.txt -P "S:\Documentaries\earths-tropical-islands-in" -f 'bv+hls-stereo_128000-English'
+.\yt-dlp "https://www.discoveryplus.in/show/life-in" --cookies .\dpcokie.txt -P "S:\Documentaries\life-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/the-ganges-with-sue-perkins-in" --cookies .\dpcokie.txt -P "S:\Documentaries\the-ganges-with-sue-perkins-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/the-birth-of-empire-the-east-india-company-in" --cookies .\dpcokie.txt -P "S:\Documentaries\the-birth-of-empire-the-east-india-company-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/ancient-empires-ceasar-in" --cookies .\dpcokie.txt -P "S:\Documentaries\ancient-empires-ceasar-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/kumbh-among-the-seekers-in" --cookies .\dpcokie.txt -P "S:\Documentaries\kumbh-among-the-seekers-in" -f 'bv+hls-stereo_128000-English'
+.\yt-dlp "https://www.discoveryplus.in/show/mankind-the-story-of-all-of-us-in" --cookies .\dpcokie.txt -P "S:\Documentaries\mankind-the-story-of-all-of-us-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/special-operations-india-in" --cookies .\dpcokie.txt -P "S:\Documentaries\special-operations-india-in" -f 'bv+hls-stereo_128000-English'
+.\yt-dlp "https://www.discoveryplus.in/show/historys-greatest-escapes-with-morgan-freeman-in" --cookies .\dpcokie.txt -P "S:\Documentaries\historys-greatest-escapes-with-morgan-freeman-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/mahakaleshwar-legends-of-shiva-in" --cookies .\dpcokie.txt -P "S:\Documentaries\mahakaleshwar-legends-of-shiva-in" -f 'bv+hls-stereo_128000-Hindi'
+.\yt-dlp "https://www.discoveryplus.in/show/abraham-lincoln-in" --cookies .\dpcokie.txt -P "S:\Documentaries\abraham-lincoln-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/the-world-wars-in" --cookies .\dpcokie.txt -P "S:\Documentaries\the-world-wars-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/kargil-valour-victory-in" --cookies .\dpcokie.txt -P "S:\Documentaries\kargil-valour-victory-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/netaji-bose-the-lost-treasure-in" --cookies .\dpcokie.txt -P "S:\Documentaries\netaji-bose-the-lost-treasure-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/ultimate-secrets-of-time-in" --cookies .\dpcokie.txt -P "S:\Documentaries\ultimate-secrets-of-time-in" -f 'bv+hls-audio-aacl-125-English'
+.\yt-dlp "https://www.discoveryplus.in/show/india-70-wonders-in" --cookies .\dpcokie.txt -P "S:\Documentaries\india-70-wonders-in" -f 'bv+hls-stereo_128000-English'
+.\yt-dlp "https://www.discoveryplus.in/show/dakshineshwar-legends-of-kali-in" --cookies .\dpcokie.txt -P "S:\Documentaries\dakshineshwar-legends-of-kali-in" -f 'bv+hls-stereo_128000-English'
+
+echo All commands executed!
+pause
